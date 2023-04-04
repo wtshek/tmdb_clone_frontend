0fe06e4cea7bfadc0730d6afa308695dbb86d9de
wtshek <wingtung.shek@accedo.tv>
2023-04-04 16:10:53 +0800

    feat: change google font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import Layout from '@/components/common/Layout/Layout';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Source_Code_Pro } from 'next/font/google';
+
+const sourceCodePro = Source_Code_Pro({ subsets: [] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main className={sourceCodePro.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
   );
 }
