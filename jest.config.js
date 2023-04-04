a1958f55f29446187faf257cebc91fac63d7d700
wtshek <wingtung.shek@accedo.tv>
2023-04-04 16:03:18 +0800

    feat: setup jest

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ const createJestConfig = nextJest({
 });
 const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
 };
 module.exports = createJestConfig(customJestConfig);
